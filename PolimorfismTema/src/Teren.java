336c976c9f8bbd2143aee0e731c71f282dfe625f
Edi-B <eduard.bulugu@emanuel.ro>
2025-05-24 11:56:00 +0300

    Add files via upload

---
+++ PolimorfismTema/src/Teren.java
@@ -0,0 +1,22 @@
+
+
+
+public class Teren extends Proprietate {
+	private int rang;
+
+
+	public Teren(Adresa adresa, String suprafata, int rang) {
+		super(adresa, suprafata);
+		this.rang = rang;
+
+	}
+
+	public double calculeazaCost() {
+		return 300 * suprafata / rang;
+
+		
+	}
+	
+	
+
+}
